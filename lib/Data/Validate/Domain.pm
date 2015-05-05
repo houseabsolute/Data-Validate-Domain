f4b3a9467803ada57d6ddbe61915cef1e9befd95
Dave Rolsky <autarch@urth.org>
2015-05-05 14:32:01 -0500

    Remove unused exporter variables

--- lib/Data/Validate/Domain.pm
+++ lib/Data/Validate/Domain.pm
@@ -9,19 +9,6 @@ require Exporter;
 
 our @ISA = qw(Exporter);
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Data::Validate::Domain ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
 our @EXPORT = qw(
 	is_domain
 	is_hostname
@@ -29,8 +16,6 @@ our @EXPORT = qw(
 );
 
 our $VERSION = '0.10';
-
-
 
 =head1 NAME
 
